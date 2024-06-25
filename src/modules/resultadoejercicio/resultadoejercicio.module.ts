862982cb68ef4b87abd572852dc5a89ed32175af
José Ferreira <36089415+JoseFerreira21@users.noreply.github.com>
2024-06-24 22:52:12 -0400

    commit mejora en post de id's PK a la tabla resultado_ejercicios

--- src/modules/resultadoejercicio/resultadoejercicio.module.ts
+++ src/modules/resultadoejercicio/resultadoejercicio.module.ts
@@ -3,9 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ResultadoEjercicios } from './entities/resultadoejercicio.entity';
 import { ResultadoEjercicioController } from './controller/resultadoejercicio.controller';
 import { ResultadoEjercicioService } from './service/resultadoejercicio.service';
+import { Ejercicios } from '../ejercicio/entities/ejercicios.entity';
+import { EjerciciosOpciones } from '../ejercicioopcion/entities/ejercicioopcion.entity';
+import { Alumnos } from '../alumno/entities/alumnos.entity';
+import { ResultadoItem } from '../resultadotestitem/entities/resultadoitem.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ResultadoEjercicios])], 
+  imports: [TypeOrmModule.forFeature([ResultadoEjercicios, Ejercicios, EjerciciosOpciones, Alumnos, ResultadoItem])], 
   controllers: [ResultadoEjercicioController],
   providers: [ResultadoEjercicioService],
   exports: [ResultadoEjercicioService],
--- src/modules/resultadoejercicio/service/resultadoejercicio.service.ts
+++ src/modules/resultadoejercicio/service/resultadoejercicio.service.ts
@@ -6,17 +6,47 @@ import { ResultadoEjercicios } from '../entities/resultadoejercicio.entity';
 import { CreateResultadoEjercicioDto } from '../dtos/resultadoejercicio.dto'; 
 import { ApiTags } from '@nestjs/swagger';
 
+import { ResultadoItem } from 'src/modules/resultadotestitem/entities/resultadoitem.entity';
+import { Ejercicios } from 'src/modules/ejercicio/entities/ejercicios.entity';
+import { EjerciciosOpciones } from 'src/modules/ejercicioopcion/entities/ejercicioopcion.entity';
+import { Alumnos } from 'src/modules/alumno/entities/alumnos.entity';
+
 @ApiTags()
 @Injectable()
 export class ResultadoEjercicioService {
   constructor(
-    @InjectRepository(ResultadoEjercicios) private resultadoEjercicio: Repository<ResultadoEjercicios>,
+    @InjectRepository(ResultadoEjercicios) private resultadoEjercicioRepository: Repository<ResultadoEjercicios>,
+    @InjectRepository(Ejercicios) private ejerciciosRepository: Repository<Ejercicios>,
+    @InjectRepository(EjerciciosOpciones) private ejerciciosOpcionesRepository: Repository<EjerciciosOpciones>,
+    @InjectRepository(Alumnos) private alumnosRepository: Repository<Alumnos>,
+    @InjectRepository(ResultadoItem) private resultadoItemRepository: Repository<ResultadoItem>,
   ) {}
 
   
-  create(data: CreateResultadoEjercicioDto) {
-    const newResultadoEjercicio = this.resultadoEjercicio.create(data);
-    return this.resultadoEjercicio.save(newResultadoEjercicio);
+  async  create(data: CreateResultadoEjercicioDto) {
+    const newResultadoEjercicio = this.resultadoEjercicioRepository.create(data);
+    
+    if (data.ejercicioId) {
+      const resultadoEjercicio = await this.ejerciciosRepository.findOne(data.ejercicioId);
+     newResultadoEjercicio.ejercicio = resultadoEjercicio;
+    }
+    
+    if (data.ejercicioOpcionesId) {
+      const resultadoEjercicioOpcion = await this.ejerciciosOpcionesRepository.findOne(data.ejercicioOpcionesId);
+     newResultadoEjercicio.ejercicioOpciones = resultadoEjercicioOpcion;
+    }
+    
+    if (data.alumnoId) {
+      const resultadoAlumno = await this.alumnosRepository.findOne(data.alumnoId);
+     newResultadoEjercicio.alumno = resultadoAlumno;
+    }
+
+    if (data.resultadoItemId) {
+      const resultadoEjercicio = await this.resultadoItemRepository.findOne(data.resultadoItemId);
+     newResultadoEjercicio.resultadoitem = resultadoEjercicio;
+    }
+    
+    return this.resultadoEjercicioRepository.save(newResultadoEjercicio);
   }
 
  
